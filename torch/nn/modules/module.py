f730c97534f4224316172d05bbb58edaaec9cace
rohithkrn <rohith.nallamaddi@gmail.com>
2019-10-17 16:36:36 -0700

    enable whole module bfloat16 conversion (#3)

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -314,6 +314,14 @@ class Module(object):
         """
         return self._apply(_if_float_tensor(lambda t: t.half()))
 
+    def bfloat16(self):
+        r"""Casts all floating point parameters and buffers to ``bfloat16`` datatype.
+
+        Returns:
+            Module: self
+        """
+        return self._apply(lambda t: t.bfloat16() if t.is_floating_point() else t)
+
     def to(self, *args, **kwargs):
         r"""Moves and/or casts the parameters and buffers.
 
